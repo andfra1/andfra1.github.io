d1b69af2c8c3c2ab0b756f2fd277b5a862a1947a
andfra <fandruszkiewicz@gmail.com>
2017-05-24 00:57:14 +0200

    optymalization?

--- data/portfolioJS.js
+++ data/portfolioJS.js
@@ -8,12 +8,68 @@ var items = document.getElementsByClassName('main-menu_items'),
     readMoreContent = document.getElementsByClassName('content_read-more'),
     cursorBlow = document.getElementById('cursorBlow');
 
+//return to default settings (default view)
+function defaultSet() {
+    line.className = '';
+    line.classList.add('line-default', 'line-text-default');
+    wrapper.style = '';
+    for (var cntnt = 0; cntnt < content.length; cntnt++) {
+        content[cntnt].style = '';
+    }
+}
+//default settings END <<<
+
+//view content
+function lineFullScreen() {
+    line.classList.remove('line-afterClick');
+    line.classList.add('line-fullScreen');
+    wrapper.style.opacity = '0.1';
+    //wrapper.style.backgroundSize = '130%';
+    wrapper.style.transitionDelay = '0.3s';
+    for (var cntnt = 0; cntnt < content.length; cntnt++) {
+        content[cntnt].style.opacity = '1';
+        content[cntnt].style.transition = '1s';
+        content[cntnt].style.transitionDelay = '0.3s';
+    }
+}
+//view content END <<<
+
+//that what elements do when user select an item from main menu (where ALL MAGIC begins!)
+function afterClick() {
+    defaultSet();
+    //changes style of line element
+    setTimeout(function () {
+        line.classList.remove('line-default');
+        line.classList.add('line-afterClick');
+    }, 500);
+    //set style block for selected content
+    if (this === document.getElementById('omnie')) {
+        content[0].style.display = 'block';
+        line.classList.add('line-text-omnie');
+    } else if (this === document.getElementById('portfolio') || this === document.getElementById('portfolio-link')) {
+        content[1].style.display = 'block';
+        line.classList.add('line-text-default');
+    } else if (this === document.getElementById('kontakt')) {
+        content[2].style.display = 'block';
+        line.classList.add('line-text-kontakt');
+    } else {
+        null;
+    }
+    setTimeout(lineFullScreen, 700);
+    return false;
+}
+for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', afterClick, false);
+}
+document.getElementById('portfolio-link').addEventListener('click', afterClick, false);
+
+
 // hamburger
 function hamburgerMenu() {
     if (hamburger.value === 'off') {
         mainMenu.style.display = 'block';
         mainMenu.addEventListener('click', function () {
-            hamburger.value = 'off';
+            hamburger.value = 'on';
             mainMenu.style.display = 'none';
         }, false);
         hamburger.value = 'on';
@@ -70,61 +126,3 @@ function readMoreToggle() {
 }
 readMoreBttn.addEventListener('click', readMoreToggle, false);
 //read more END <<<
-
-
-//return to default settings (default view)
-function defaultSet() {
-    line.className = '';
-    line.classList.add('line-default', 'line-text-default');
-    wrapper.style = '';
-    for (var cntnt = 0; cntnt < content.length; cntnt++) {
-        content[cntnt].style = '';
-    }
-    return false;
-}
-//default settings END <<<
-
-//view content
-function lineFullScreen() {
-    line.classList.remove('line-afterClick');
-    line.classList.add('line-fullScreen');
-    wrapper.style.backgroundSize = '130%';
-    wrapper.style.transitionDelay = '0.3s';
-    for (var cntnt = 0; cntnt < content.length; cntnt++) {
-        content[cntnt].style.opacity = '1';
-        content[cntnt].style.transition = '1s';
-        content[cntnt].style.transitionDelay = '0.3s';
-    }
-    return false;
-}
-//view content END <<<
-
-//that what elements do when user select an item from main menu (where ALL MAGIC begins!)
-function afterClick() {
-    defaultSet();
-    //hamburgerMenu('off'); //hides menu when roll-over
-    //changes style of line element
-    setTimeout(function () {
-        line.classList.remove('line-default');
-        line.classList.add('line-afterClick');
-    }, 300);
-    //set style block for selected content
-    if (this === document.getElementById('omnie')) {
-        content[0].style.display = 'block';
-        line.classList.add('line-text-omnie');
-    } else if (this === document.getElementById('portfolio') || this === document.getElementById('portfolio-link')) {
-        content[1].style.display = 'block';
-        line.classList.add('line-text-default');
-    } else if (this === document.getElementById('kontakt')) {
-        content[2].style.display = 'block';
-        line.classList.add('line-text-kontakt');
-    } else {
-        null;
-    }
-    setTimeout(lineFullScreen, 500);
-    return false;
-}
-for (var i = 0; i < items.length; i++) {
-    items[i].addEventListener('click', afterClick, false);
-}
-document.getElementById('portfolio-link').addEventListener('click', afterClick, false);
